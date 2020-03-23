7f1dce58aa471b5dd8f1902b869e5b67bcb00077
user <maxkao63@gmail.com>
2020-03-23 14:01:57 +0800

    fix

--- push.js
+++ push.js
@@ -7,6 +7,15 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:test@code.co.tw' , vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {};
+  let sub = {
+      endpoint:
+      "https://fcm.googleapis.com/fcm/send/fh51Q1lRWr0:APA91bHW0txpvenKaZdCatwsiUEw0lo8Ch-0YgzDufQLFrgsHaOvSjiGctKofHXx5rHzlbsi-wVZzsasH6Ye_d9n6OMAzSD38uCM-jZKB4y9UNnXZR9uZWy-cA_euZSfxrwrygI2LJT-",
+      expirationTime: null,
+      keys: {
+        p256dh:
+          "BH4jy37w9U05ywVdj8_AP87LYTyhawLPH-V6XqZppnECYuq0kOy7UAxqH5drTVGnsy3oiTbRxU9sgUW4jxpxw-I",
+          auth:"xux_PJqniSjm98VDClfanA"
+        }
+        }
   
   push.sendNotification(sub, 'OMG　ＧＧ!')
